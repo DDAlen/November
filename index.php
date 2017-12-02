56e7d45e9d2dc01916cb57d4a71515353cd51836
huang_wentai <huang_wentai@126.com>
2017-12-02 11:36:03 +0800

    index.php7添加对数据库的连接

--- index.php
+++ index.php
@@ -2,4 +2,14 @@
 echo 'hello word';
 echo '<br />';
 echo mt_rand();
+
+
+$conn=mysqli_connect('192.168.0.106:3306','root','password',false);
+echo 22;
+if ($conn){
+  echo "LNMP platform connect to mysql is successful!";
+}else{
+  echo "LNMP platform connect to mysql is failed!";
+}
+
 ?>
