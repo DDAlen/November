c4235e4ff10e7ada6fed38a565a1c8153740bf66
Huang <huang_wentai@126.com>
2017-12-13 08:55:55 +0800

    think

---
+++ MyPHP/config.php
@@ -0,0 +1,13 @@
+<?php
+define('MYSQLUSER', 'root');
+
+#define('HOST', '192.168.0.106:3306');
+#define('PASWORD', 'password');
+
+
+define('HOST', 'localhost');
+define('PASWORD', 'root');
+
+define('DOCUMENTROOT', $_SERVER['DOCUMENT_ROOT']);
+
+?>
